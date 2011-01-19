4eb64df474a2d139a2299a1a6d66c23ee1fe0a23
Adrian Olaru <agolaru@gmail.com>
2011-01-19 16:16:06 +0200

    notes

--- public_html/worker.js
+++ public_html/worker.js
@@ -1,7 +1,7 @@
 addEventListener('message', function(e) {
   var n = parseInt(e.data, 10);
   postMessage(fact(n));
-}, true);
+}, false);
 
 //TIP: you still have to watch out for CPU intensive calculation
 
