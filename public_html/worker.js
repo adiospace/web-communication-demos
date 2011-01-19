596c3fbd4ebcdc0983f02aa36ea70c006fb633ab
Adrian Olaru <agolaru@gmail.com>
2011-01-19 13:15:45 +0200

    web workers

--- public_html/worker.js
+++ public_html/worker.js
@@ -1,7 +1,10 @@
-addEventListener('message', function (e) {
-  var r = 'nu';
-  if (EventSource) { 
-    r = 'da'; 
-  } 
-  postMessage(r);
+addEventListener('message', function(e) {
+  var n = parseInt(e.data, 10);
+  postMessage(fact(n));
 }, true);
+
+//TIP: you still have to watch out for CPU intensive calculation
+
+function fact(n) {
+  return n ? n * fact(n-1) : 1;
+}
