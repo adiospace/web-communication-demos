5c5235f6564f8d3ec7d7b24d6ce4e1d8827e1737
Adrian Olaru <agolaru@gmail.com>
2011-01-19 14:27:02 +0200

    web sockets

--- ws_server.js
+++ ws_server.js
@@ -1,22 +1,21 @@
-var sys = require("sys"),
-    ws = require("./libs/ws");
+var ws = require("./libs/ws");
 
 ws.createServer(function (ws) {
   ws.addListener("connect", function (resource) { 
     // emitted after handshake
-    sys.debug("connect: " + resource);
+    console.log("connect: " + resource);
 
     // server closes connection after 3 min, will also get "close" event
     setTimeout(ws.end, 10 * 60 * 1000); 
   }).addListener("data", function (data) { 
     // handle incoming data
-    sys.debug(data);
+    console.log(data);
 
     // send data to client
-    ws.write("Thanks!");
+    ws.write(data);
   }).addListener("close", function () { 
     // emitted when server or client closes connection
-    sys.debug("close");
+    console.log("close");
   });
 }).listen(4001);
 console.log('Web Socket Server running at http://127.0.0.1:4001');
